c29b2b23ddd0e1459cf48b0833b21d258eeb6659
David <dav1dch@outlook.com>
2023-10-31 22:12:30 +0800

    comment

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from model_factory import model_factory
 model = model_factory()
 model = model.cuda()
 
+# batch_size, sequence ( 3 images as 1 sequence ), channel (rgb), height, width
 batch_sample = torch.rand((4, 3, 3, 480, 640)).cuda()
 
 model(batch_sample)
